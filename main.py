b0b4ac5c7b782532fe07e75d111684a90efa000b
Ryan Bartlett <ryanzbartlett@gmail.com>
2025-05-07 21:17:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import constants
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
